5ffbf53bce24ab65cf84fed2372a19cab0d87493
Tozzger <997053715@qq.com>
2021-03-27 22:18:46 +0800

    alpha 0.2

--- client/src/fzu/zrf/mtsys/client/gui/Member.java
+++ client/src/fzu/zrf/mtsys/client/gui/Member.java
@@ -1,9 +1,16 @@
 package fzu.zrf.mtsys.client.gui;
 
+import fzu.zrf.mtsys.net.Login.Result;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Member extends Application {
+
+    private final Result result;
+
+    public Member(Result ret) {
+        result = ret;
+    }
 
     @Override
     public void start(Stage var1) throws Exception {
